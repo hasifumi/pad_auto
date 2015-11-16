a0b9087f3fea19c79430a928786c49a6c9a8774a
Fumio Hashidume <hassy@air.ne.jp>
2015-11-16 23:10:10 +0900

    modify

--- pad.py
+++ pad.py
@@ -3,11 +3,12 @@
 import random
 import copy
 
-def create_drops_random(num=30):
+def create_drops_random(w=6, h=5, d="rbgldc"):
     lst = []
-    random_srt = "rbgldc"  # r:red, b:blue, g:green, l:light, d:dark, c:cure
-    for i in range(num):
-        lst.append(random.choice(random_srt))
+    #random_srt = "rbgldc"  # r:red, b:blue, g:green, l:light, d:dark, c:cure
+    for i in range(w*h):
+        #lst.append(random.choice(random_srt))
+        lst.append(random.choice(d))
     return lst
 
 def pivot_drops(drops_lst):
@@ -63,15 +64,78 @@ def create_goal_drops(sorted_drops_lst, max_combo):
     #return [lst, lst2]
     return lst2
 
-#lst = create_drops_random()
+def isEqual_list(lst1, lst2):
+    if len(lst1) != len(lst2):
+        return False
+    if len(lst1) == 0:
+        return False
+    ret = True
+    for i in range(len(lst1)):
+        if lst1[i] != lst2[i]:
+            ret = False
+            break
+    return ret
+
+adjacent_3x2 = ((1, 3),         # 0
+                (0, 2, 4),      # 1
+                (1, 5),         # 2
+                (0, 4),         # 3
+                (1, 3, 5),      # 4
+                (2, 4))         # 5
+
+def exchange(lst, ex1, ex2):  # 0:first_item
+    if len(lst) == 0:
+        return lst
+    if (ex1 + 1 > len(lst)) or (ex2 + 1 > len(lst)):
+        return lst
+    if ex1 == ex2:
+        return lst
+    tmp_lst = lst[:]
+    tmp = tmp_lst[ex1]
+    tmp_lst[ex1] = tmp_lst[ex2]
+    tmp_lst[ex2] = tmp
+    return tmp_lst
+
+
+
+def search(goal_drops, path, prev_drops, adjacent):
+    if isEqual_list(goal_drops, prev_drops):
+        print path
+        print prev_drops
+        return
+    n = path[len(path) - 1]
+    for x in adjacent[n]:
+        if x not in path:
+            #print "n:" + str(n) + ", x:" + str(x) + ", path:" + str(path)
+            ##print "prev_drops[n]:" + str(prev_drops[n]) + ", prev_drops[x]:" + str(prev_drops[x])
+            #print "prev_drops:" + str(prev_drops) + ", next_drops:" + str(exchange(prev_drops, x, n))
+            path.append(x)
+            search(goal_drops, path, exchange(prev_drops, x, n), adjacent)
+            path.pop()
+
+
+#lst = create_drops_random(4, 3, "bgr")
 #dct = pivot_drops(lst)
 #max_cmb = max_combo(dct)
 #lst2 = sort_drops(dct)
 #lst3 = create_goal_drops(lst2, max_cmb)
 #
 #print lst
+#print len(lst)
 #print dct
 #print max_cmb
 #print lst2
 #print lst3
 ##print len(lst3[1])
+
+#lst1 = "abc"
+#lst2 = "abc"
+#print isEqual_list(lst1, lst2)
+
+lst1 = ["a", "b", "a", "c", "a", "a"]
+lst2 = ["a", "c", "b", "a", "a", "a"]
+
+for x in range(len(lst2)):
+    search(lst1, [x], lst2, adjacent_3x2)
+
+#print exchange(["a", "b", "c", "d", "e"], 3, 2)
--- pad_thinter.py
+++ pad_thinter.py
@@ -2,22 +2,24 @@ import sys
 import Tkinter
 import pad
 
+width = 7
+height = 6
 circle_height = 50
 
 root = Tkinter.Tk()
 root.title(u'Software Title')
-root.geometry(str(6*circle_height) + "x" + str(5*2*circle_height+50))
+root.geometry(str(width * circle_height) + "x" + str(height * 2 * circle_height + 50))
 
 #Canvas1
-c1 = Tkinter.Canvas(root, width=6*circle_height, height=5*2*circle_height+50)
+c1 = Tkinter.Canvas(root, width=width*circle_height, height=height*2*circle_height+50)
 c1.place(x=1, y=1)
 
 c1_circles = []
 c2_circles = []
 
 def draw_circles(circles, x, y):
-    for j in range(5):
-        for i in range(6):
+    for j in range(height):
+        for i in range(width):
             #circles.append(c1.create_oval(1+j*circle_height, 1+i*circle_height, (j+1)*circle_height, (i+1)*circle_height))
             circles.append(c1.create_oval(x+i*circle_height, y+j*circle_height, x+(i+1)*circle_height, y+(j+1)*circle_height))
 
@@ -36,14 +38,14 @@ def fill_circles(circles, lst, cvs):
         elif lst[index] == 'c':
             cvs.itemconfig(circles[index], fill='pink')
 
-lst1 = pad.create_drops_random()
+lst1 = pad.create_drops_random(width, height, "bgr")
 dct = pad.pivot_drops(lst1)
 max_cmb = pad.max_combo(dct)
 lst2 = pad.sort_drops(dct)
 lst = pad.create_goal_drops(lst2, max_cmb)
 
 draw_circles(c1_circles, 1, 1)
-draw_circles(c2_circles, 1, 5*circle_height+51)
+draw_circles(c2_circles, 1, height*circle_height+51)
 fill_circles(c1_circles, lst1, c1)
 fill_circles(c2_circles, lst, c1)
 
