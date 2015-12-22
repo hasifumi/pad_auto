b5f23ac5c5582e35c1df899dc83c821a814476f3
Fumio Hashidume <hassy@air.ne.jp>
2015-12-23 06:37:35 +0900

    simplify pad_auto

--- pad_auto.py
+++ pad_auto.py
@@ -65,98 +65,92 @@ def print_board(width, height, board):# {{{
         print board[h*width:h*width+width]
     return 1# }}}
 
+device_path = "/sdcard/screen.png"
+def get_screenshot(device_path):# {{{
+    screencap_cmd = ["adb", "shell", "screencap", device_path]
+    subprocess.check_call(screencap_cmd, shell=True)
+
+    pull_cmd = ["adb", "pull", device_path]
+    subprocess.check_call(pull_cmd, shell=True)
+
+    return# }}}
+
+def is_nexus(path):# {{{
+    pic = Image.open(path, 'r')
+    if pic.width == 800:
+        return True
+    else:
+        return False# }}}
+
+def idx2xy(width, idx):# {{{
+    return[int(idx/width), int(idx%width)]# }}}
+
+def conv_x(i, is_nexus):# {{{
+    if is_nexus:
+        return 15 + 65 + 130 * (int(i))
+    else:
+        return 5 + 90 + 180 * (int(i))# }}}
+
+def conv_y(i, is_nexus):# {{{
+    if is_nexus:
+        return 560 + 65 + 130 * (int(i))
+    else:
+        return 850 + 90 + 180 * (int(i))# }}}
+
+def calc_i(flag, ary, is_nexus):# {{{
+    pos_i = "\""
+    for i,v in enumerate(ary):
+        if flag == "x":
+            pos_i += str(conv_x(ary[i], is_nexus))
+        else:
+            pos_i += str(conv_y(ary[i], is_nexus))
+        pos_i += ","
+    pos_i = pos_i.rstrip(",")
+    pos_i += "\""
+    return pos_i# }}}
+
+def get_route(route, is_nexus):# {{{
+    x = []
+    y = []
+    for r in route:
+        ans = idx2xy(WIDTH, r)
+        x.append(ans[1])
+        y.append(ans[0])
+    pos_x = calc_i("x", x, is_nexus)
+    pos_y = calc_i("y", y, is_nexus)
+    return (pos_x, pos_y)# }}}
+
+def move_drop(pos_x, pos_y, swipe_time):# {{{
+    uiautomator_cmd = ["adb", "shell", "uiautomator", "runtest", "UiAutomator.jar", "-c", "com.hahahassy.android.UiAutomator#swipe", "-e",  "\"x\"", pos_x, "-e","\"y\"", pos_y, "-e","\"t\"", swipe_time]
+    subprocess.check_call(uiautomator_cmd, shell=True)# }}}
+
+path = "C:/Users/" + WIN_USER_NAME + "/MyProject/python/pad_auto/screen.png"
+# main routine
+
 start_time = time.time()
 
-screencap_cmd = ["adb", "shell", "screencap", "/sdcard/screen.png"]
-subprocess.check_call(screencap_cmd, shell=True)
+get_screenshot(device_path)
 
-pull_cmd = ["adb", "pull", "/sdcard/screen.png"]
-subprocess.check_call(pull_cmd, shell=True)
+board = pazdracombo.convert_h_w(padboard.check_board(".\screen.png", WIDTH, HEIGHT, 0))
 
-#path = "C:/Users/hassy/MyProject/python/pad_auto/screen.png" # Win10
-#path = "C:/Users/fumio/MyProject/python/pad_auto/screen.png"  # Win7
-path = "C:/Users/" + WIN_USER_NAME + "/MyProject/python/pad_auto/screen.png"
-
-lap1_time = time.time()
-
-pic = Image.open(path, 'r')
-if pic.width == 800:# {{{
-    is_nexus = True
-else:
-    is_nexus = False# }}}
-
-#board = padboard.check_board(".\screen.png", 6, 5)
-temp_board = padboard.check_board(".\screen.png", WIDTH, HEIGHT, 0)
-
-board = pazdracombo.convert_h_w(temp_board)
+n_best = pad_search.Nbeam(WIDTH, HEIGHT, board, MAX_TURN, PLAYNUM, PARMS)
+pos_x, pos_y = get_route(n_best.route, is_nexus(path))
 
 # 確認用
 print "[board]"
 print print_board(WIDTH, HEIGHT, board)
 print ""
 
-x = []
-y = []
-
-def idx2xy(width, idx):# {{{
-    return[int(idx/width), int(idx%width)]# }}}
-
-#n_best = pad_search.Nbeam(6, 5, board, MAX_TURN, PLAYNUM, RED, BLUE, GREEN, LIGHT, DARK, CURE)
-n_best = pad_search.Nbeam(WIDTH, HEIGHT, board, MAX_TURN, PLAYNUM, PARMS)
-
-## 確認用
 print "[combo]"
 print print_board(WIDTH, HEIGHT, n_best.board)
 print ""
-
-for r in n_best.route:# {{{
-    ans = idx2xy(WIDTH, r)
-    x.append(ans[1])
-    y.append(ans[0])# }}}
-
-def conv_x(i):# {{{
-    if is_nexus:
-        return 15 + 65 + 130 * (int(i))
-    else:
-        return 5 + 90 + 180 * (int(i))# }}}
-
-def conv_y(i):# {{{
-    if is_nexus:
-        return 560 + 65 + 130 * (int(i))
-    else:
-        return 850 + 90 + 180 * (int(i))# }}}
-
-def calc_i(flag, ary):# {{{
-    pos_i = "\""
-    for i,v in enumerate(ary):
-        if flag == "x":
-            pos_i += str(conv_x(ary[i]))
-        else:
-            pos_i += str(conv_y(ary[i]))
-        pos_i += ","
-    pos_i = pos_i.rstrip(",")
-    pos_i += "\""
-    return pos_i# }}}
-
-pos_x = calc_i("x", x)
-pos_y = calc_i("y", y)
-
-#print pos_x
-#print pos_y
 
 #print "press any key"
 #input_test_word = raw_input(">>>  ")
 #print "key: " + str(input_test_word)
 
-swipe_time = str(SWIPE)
-
-uiautomator_cmd = ["adb", "shell", "uiautomator", "runtest", "UiAutomator.jar", "-c", "com.hahahassy.android.UiAutomator#swipe", "-e",  "\"x\"", pos_x, "-e","\"y\"", pos_y, "-e","\"t\"", swipe_time]
-
-
-subprocess.check_call(uiautomator_cmd, shell=True)
+move_drop(pos_x, pos_y, str(SWIPE))
 
 elapsed_lap1_time = lap1_time - start_time
 elapsed_time = time.time() - start_time
-
-#print("lap1_time:{0}".format(elapsed_lap1_time)) + "[sec]"
 print("elapsed_time:{0}".format(elapsed_time)) + "[sec]"
